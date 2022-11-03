947889e0fa87c7b679cabf1c83b151b0bdcd7954
The Unbearables <117344043+pesadelos@users.noreply.github.com>
2022-11-03 09:59:54 +0100

    minha_idade.py
    É um script simples que o vai ajudar a saber quantos anos tem de acordo com o ano em que se encontra caso tenha problemas com matemática kkkk

--- minha_idade.py
+++ minha_idade.py
@@ -1,6 +1,6 @@
 # minha_idade
 
-print("__________🎭The Unbearable🎭__________")
+print("__________🎭The Unbearables🎭__________")
 
 nome = input("Digite seu nome \n")
 idade = int(input("Digite seu ano de nascimento\n"))
